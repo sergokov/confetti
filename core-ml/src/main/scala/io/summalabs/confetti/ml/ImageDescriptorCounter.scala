d6f2c5344354a2cbe1017d6222b3d75b36d4bccc
sergokov <sergokov@gmail.com>
2015-10-26 22:11:46 +0300

    Add images to sequence file 14

--- core-ml/src/main/scala/io/summalabs/confetti/ml/ImageDescriptorCounter.scala
+++ core-ml/src/main/scala/io/summalabs/confetti/ml/ImageDescriptorCounter.scala
@@ -3,6 +3,7 @@ package io.summalabs.confetti.ml
 import org.apache.hadoop.io.{BytesWritable, Text}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkContext, SparkConf}
+import org.opencv.core.Core
 
 /**
  * @author Sergey Kovalev.
@@ -16,6 +17,8 @@ object ImageDescriptorCounter {
     val file = sc.sequenceFile(args(0), classOf[Text], classOf[BytesWritable])
 
     val imagesDescriptors: RDD[(String, Descriptor)] = file.map{image => {
+      System.loadLibrary(Core.NATIVE_LIBRARY_NAME)
+
       val text = image._1.asInstanceOf[Text]
       val imageBytes = image._2.asInstanceOf[BytesWritable]
       val descriptor: Descriptor = Descriptor.buildDscFromRawData(imageBytes.getBytes, Descriptor.DEF_BIN_NUMBER, true)
